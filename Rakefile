493da537eaeff0d9a45feec981bda31ff3c5df59
Scott Clark <scottclark11@gmail.com>
2015-03-08 19:55:54 -0400

    maybe this will work?

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 require 'bundler/gem_tasks'
-require 'rake/testtask'
 require 'coveralls/rake/task'
+require 'rake/testtask'
 
 Coveralls::RakeTask.new
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,14 @@
+require 'coveralls'
+Coveralls.wear!
+
 require 'simplecov'
 SimpleCov.start { add_filter '/test/' }
 
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/spec'
-require 'coveralls'
 
-Coveralls.wear!
-
-Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 
 require 'accessible'
