a5d30bc95a0a4a5f83e7adfcf276bcd948705598
Scott Clark <scottclark11@gmail.com>
2015-03-08 20:26:50 -0400

    wear Coveralls after starting SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,12 @@
 require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
-
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/spec'
 
 Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
-
 SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-SimpleCov.start do
-   add_filter '/test/'
-end
+SimpleCov.start { add_filter '/test/' }
+Coveralls.wear!
 
 require 'accessible'
